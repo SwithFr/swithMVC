21f73c83b480c991a966bfd2fd3503db9e10bc28
Jeremy Smith <contact@swith.fr>
2015-03-05 19:14:45 +0100

    fix syntaxe

--- Core/Lib/functions.php
+++ Core/Lib/functions.php
@@ -1,21 +1,22 @@
-<?php 
+<?php
 
 /**
  * "Foure tout"
- * FONCTIONS UTILES UN PEU PARTOUT 
+ * FONCTIONS UTILES UN PEU PARTOUT
  */
 
 /**
  * Créer un petit débug
- * @param  var|array  $var la/les variable(s) à debugger
+ * @param  var|array $var la/les variable(s) à debugger
  * @param  boolean $die Doit-on couper le script ?
  */
-function debug($var,$die = true){
+function debug($var, $die = true)
+{
 
     if ($_ENV['DEBUG'])
-	echo "<pre>".var_dump($var)."</pre>";
+        echo "<pre>" . var_dump($var) . "</pre>";
 
-	if($die)
-		die();
+    if ($die)
+        die();
 
 }
