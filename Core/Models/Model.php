e11ef322fe8085769280cd7c3ecc055eaefc30a5
Jeremy Smith <contact@swith.fr>
2015-04-20 18:50:16 +0200

    add proprerty Entity

--- Core/Models/Model.php
+++ Core/Models/Model.php
@@ -50,6 +50,12 @@ class Model
      */
     public $connexions = [];
 
+    /**
+     * L'entité correspondant au model
+     * @var null
+     */
+    public $Entity = null;
+
     /*
         METHODES
      */
@@ -61,6 +67,13 @@ class Model
 
         // Initialisation des variables
         $this->setNameTableAndModel();
+
+        // On instancie l'entity si besoin
+        if ($_ENV['DB_OPTION_FETCH_MODE'] == 'PDO::FETCH_CLASS') {
+            $entityName = '\App\Models\Entities\\' . $this->name . 'Entity';
+            if(class_exists($entityName) && !is_null($this->Entity))
+                $this->Entity = new $entityName();
+        }
 
         if (is_null($this->bdd))
             $this->bdd = DbProvider::getDb();
