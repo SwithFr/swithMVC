06d683c55c4598757b4e2075052a3595170c37fc
Jeremy Smith <contact@swith.fr>
2015-04-20 19:57:24 +0200

    update function search

--- Core/Models/Behaviors/Search.php
+++ Core/Models/Behaviors/Search.php
@@ -4,30 +4,46 @@
 namespace Core\Models\Behaviors;
 
 
-trait Search {
-
+trait Search
+{
     /**
      * Permet d'effectuer une chercher
      * @param  string $what ce que l'on doit chercher
-     * @param  array $where sur quel champs
-     * @param string $get ce que l'on veut récupérer
+     * @param array|arrayt $where sur quel champs
+     * @param string $get
      * @param null $table
      * @param  string $how Comment doit on chercher [around|exactly]
      * @return mixed
      */
     public function search($what, Array $where, $get = '*', $table = null, $how = "around")
     {
-
         if (is_null($table))
             $table = $this->table;
 
-        if ($table == "books") {
-            $query = "SELECT DISTINCT $get FROM $table
-                      JOIN book_library ON book_id = books.id
-                      JOIN libraries ON library_id = libraries.id WHERE
-                      private = 0 AND (";
-        } else {
-            $query = "SELECT DISTINCT $get FROM $table WHERE ";
+        if(is_string($get))
+            $query = 'SELECT ' . $get . ' FROM ' . $table . ' WHERE ';
+        elseif(is_array($get)) {
+            $query = 'SELECT ';
+            // Si on a des champs définis
+            if (isset($get['fields']))
+                $query .= $get['fields'];
+
+
+            $query .= " FROM " . $table;
+
+            // Si on doit faire un join
+            if (isset($get['joins'])) {
+                $joins = [];
+                foreach ($get['joins'] as $j) {
+                    if (!isset($this->joins) || !isset($this->joins[$j])) {
+                        debug("Le model " . $this->name . " n'a pas d'association avec la table $j ! Veuillez créer un tableau public \$joins dans votre model " . $this->name, false);
+                    } else {
+                        $joins[] = " JOIN $j ON $j.{$this->primaryKey} = {$this->table}." . $this->joins[$j];
+                    }
+                }
+                $query .= implode(" AND ", $joins) . ' WHERE ';
+            }
+
         }
 
         $params = [];
@@ -39,6 +55,7 @@ trait Search {
 
         foreach ($where as $c) {
             foreach ($what as $w) {
+                $w = trim($this->bdd->quote($w),'\'');
                 if ($how === "around") {
                     $params[] = $c . " LIKE " . "'%$w%'";
                 } elseif ($how === "exactly") {
@@ -46,14 +63,14 @@ trait Search {
                 }
             }
         }
-
         $params = implode(" OR ", $params);
-        if ($table == 'books')
-            $params .= ')';
 
-        $req = $this->db->query($query . $params);
+        $req = $this->bdd->query($query . $params);
 
-        return $req->fetchAll();
+        if ($_ENV['DB_OPTION_FETCH_MODE'] == 'PDO::FETCH_CLASS')
+            return $req->fetchAll(\PDO::FETCH_CLASS, 'App\\Models\\Entities\\' . $table . 'Entity');
+        else
+            return $req->fetchAll();
     }
 
     /**
