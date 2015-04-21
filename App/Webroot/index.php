ed850e0ca979f2fcbd7f099ccdcef8bc2419eed4
Jeremy Smith <contact@swith.fr>
2015-04-21 19:26:27 +0200

    fix syntaxe

--- App/Webroot/index.php
+++ App/Webroot/index.php
@@ -1,22 +1,21 @@
 <?php
-	
-	/**
-	 * Définition des contantes de base
-	 */
-	define('DS'     , DIRECTORY_SEPARATOR);
-	define('BASE'   , dirname(dirname(__DIR__)));
-	define('ROOT'   , dirname(dirname(str_replace('index.php', '', $_SERVER['SCRIPT_NAME']))).DS);
-	define('CORE'   , ROOT.'Core'.DS); 
-	define('APP'    , ROOT.'App'.DS);
-	define('WEBROOT', APP."Webroot".DS);
 
-	/**
-	 * On charge l'autoloader de composer
-	 */
-    require_once BASE.DS."vendor".DS."autoload.php";
-    require_once BASE.DS."Core".DS.'Lib'.DS.'functions.php';
+    /**
+     * Définition des contantes de base
+     */
+    define('DS', DIRECTORY_SEPARATOR);
+    define('BASE', dirname(dirname(__DIR__)));
+    define('ROOT', dirname(dirname(str_replace('index.php', '', $_SERVER['SCRIPT_NAME']))) . DS);
+    define('CORE', ROOT . 'Core' . DS);
+    define('APP', ROOT . 'App' . DS);
+    define('WEBROOT', APP . "Webroot" . DS);
+
+    /**
+     * On charge l'autoloader de composer
+     */
+    require_once BASE . DS . "vendor" . DS . "autoload.php";
+    require_once BASE . DS . "Core" . DS . 'Lib' . DS . 'functions.php';
 
     use Core\Dispatcher;
 
     $dispatcher = new Dispatcher();
-    
