8e32cbe4369337084fe660c9c2d9025efe935e1e
Jeremy Smith <contact@swith.fr>
2015-04-23 21:01:50 +0200

    add helper Paginator

---
+++ Core/Helpers/Paginator.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Core\Helpers;
+
+
+class Paginator 
+{
+    /**
+     * Créer une petite pagination
+     * @param $paginateResults
+     * @return string
+     */
+    public static function paginate($paginateResults){
+        $html = '<ul class="pagination">';
+        for($i=1; $i <= $paginateResults['nbPages']; $i++){
+            $html .=  "<li><a href='?paginate=$i'>$i</a></li>";
+        }
+        $html .= '</ul>';
+        return $html;
+    }
+} 
