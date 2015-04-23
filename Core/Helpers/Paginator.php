dc26faea024fea4b3eeda4c0c39fa197880e4fe1
Jeremy Smith <contact@swith.fr>
2015-04-23 21:02:12 +0200

    fix syntaxe

--- Core/Helpers/Paginator.php
+++ Core/Helpers/Paginator.php
@@ -4,17 +4,18 @@
 namespace Core\Helpers;
 
 
-class Paginator 
+class Paginator
 {
     /**
      * Créer une petite pagination
      * @param $paginateResults
      * @return string
      */
-    public static function paginate($paginateResults){
+    public static function paginate($paginateResults)
+    {
         $html = '<ul class="pagination">';
-        for($i=1; $i <= $paginateResults['nbPages']; $i++){
-            $html .=  "<li><a href='?paginate=$i'>$i</a></li>";
+        for ($i = 1; $i <= $paginateResults['nbPages']; $i++) {
+            $html .= "<li><a href='?paginate=$i'>$i</a></li>";
         }
         $html .= '</ul>';
         return $html;
