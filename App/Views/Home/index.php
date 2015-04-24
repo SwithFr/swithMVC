4e972f6a74fe127a514694370051bc4b91bc6d74
Jeremy Smith <contact@swith.fr>
2015-04-24 18:33:01 +0200

    add some text on homepage

--- App/Views/Home/index.php
+++ App/Views/Home/index.php
@@ -1,2 +1,3 @@
 <?php $title_for_layout = "Bienvenue sur le Framework Swith"; ?>
 <h1 class="title--center">Bienvenue ! Bonne utilisation de Swith Framework !</h1>
+<p class="doc-help">Si vous avez besoin d'aide, consultez la documentation ici : <?= \Core\Helpers\Html::url('http://mvc.swith.fr/pages/documentation/commencer','Documentation',null,['class'=>'doc-link']); ?></p>
