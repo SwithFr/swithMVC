225cf8c1f4865f106d52d7f56e4ab961a36157cc
Jeremy Smith <contact@swith.fr>
2015-04-27 20:57:58 +0200

    update debug

--- Core/Lib/functions.php
+++ Core/Lib/functions.php
@@ -13,10 +13,16 @@
 function debug($var, $die = true)
 {
 
-    if ($_ENV['DEBUG'])
-        echo "<pre>" . var_dump($var) . "</pre>";
+    if ($_ENV['DEBUG']) {
+        $debug = debug_backtrace();
+        echo "<p><strong>" . $debug[0]['file'] . " ligne : " . $debug[0]['line'] . "</strong></p>";
+        echo "<pre>";
+        var_dump($var);
+        echo "</pre>";
+    }
 
-    if ($die)
+    if ($die) {
         die();
+    }
 
 }
