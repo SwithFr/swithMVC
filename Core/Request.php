396fe7a33e09ab7ee83bbc56f01ba63c1741206f
Jeremy Smith <contact@swith.fr>
2015-06-21 16:17:08 +0200

    syntaxe

--- Core/Request.php
+++ Core/Request.php
@@ -7,15 +7,15 @@ use App\Config\App;
 class Request
 {
 
-    public $url,          // url appellée par l'utilisateur
-        $action,          // Action demandée
-        $controller,      // Controller demandé
-        $referer = null,  // Page précédante
-        $params,          // Les paramettres requis
-        $prefixe = false, // Le prefixe requis si besoin
-        $isPost = false,  // La methode est de type post ?
-        $data = false,    // Les données postées
-        $page = 1;        // Pour la pagination
+    public $url,             // url appellée par l'utilisateur
+           $action,          // Action demandée
+           $controller,      // Controller demandé
+           $referer = null,  // Page précédante
+           $params,          // Les paramettres requis
+           $prefixe = false, // Le prefixe requis si besoin
+           $isPost = false,  // La methode est de type post ?
+           $data = false,    // Les données postées
+           $page = 1;        // Pour la pagination
 
     public function __construct()
     {
@@ -25,12 +25,12 @@ class Request
         if (isset($_SERVER['PATH_INFO'])) {
             $this->url = $_SERVER['PATH_INFO'];
         } else {
-            $this->url = ( !is_null($default_controller) ? $default_controller : "Controller" ) . "/"  . $app->getAppSettings("default_action");
+            $this->url = (!is_null($default_controller) ? $default_controller : "Controller") . "/" . $app->getAppSettings("default_action");
         }
 
-        if(isset($_SERVER['HTTP_REFERER'])) {
-            $root = addcslashes(ROOT,'/');
-            $parts = preg_split('/'.$root.'/',$_SERVER['HTTP_REFERER']);
+        if (isset($_SERVER['HTTP_REFERER'])) {
+            $root = addcslashes(ROOT, '/');
+            $parts = preg_split('/' . $root . '/', $_SERVER['HTTP_REFERER']);
             $this->referer = isset($parts[1]) ? $parts[1] : $parts[0];
         }
 
@@ -43,8 +43,8 @@ class Request
             unset($_POST);
         }
 
-        if (isset($_GET['paginate']) && is_numeric($_GET['paginate']) && $_GET['paginate'] > 0) {
-            $this->page = round($_GET['paginate']);
+        if (isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] > 0) {
+            $this->page = round($_GET['page']);
         }
     }
 
