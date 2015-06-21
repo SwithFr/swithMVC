c3e929f3f4ec855631bd6f93cf03a092f2acc834
Jeremy Smith <contact@swith.fr>
2015-06-21 16:07:28 +0200

    remove url method from Html helper

--- App/Views/Home/index.php
+++ App/Views/Home/index.php
@@ -1,3 +1,4 @@
 <?php $title_for_layout = "Bienvenue sur le Framework Swith"; ?>
 <h1 class="title--center">Bienvenue ! Bonne utilisation de Swith Framework !</h1>
-<p class="doc-help">Si vous avez besoin d'aide, consultez la documentation ici : <?= \Core\Helpers\Html::url('http://mvc.swith.fr/pages/documentation/commencer','Documentation',null,['class'=>'doc-link']); ?></p>
+<p class="doc-help">Si vous avez besoin d'aide, consultez la documentation ici : <a
+        href="http://mvc.swith.fr/pages/documentation/commencer" class="doc-link">Documentation</a></p>
