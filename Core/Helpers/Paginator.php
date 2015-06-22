143f6c41502b4cb2bbe8af5e50400d0a62ac2eca
Jeremy Smith <contact@swith.fr>
2015-06-22 15:57:36 +0200

    update Paginator

--- Core/Helpers/Paginator.php
+++ Core/Helpers/Paginator.php
@@ -8,16 +8,64 @@ class Paginator
 {
     /**
      * Créer une petite pagination
+     * Si $previousAndNext est à true, on a en plus les liens suivant et précédant
      * @param int $nbPages
+     * @param bool $previousAndNext
+     * @param bool $active
      * @return string
      */
-    public static function paginate($nbPages)
+    public static function paginate($nbPages, $previousAndNext = false, $active = true)
     {
-        $html = '<ul>';
+        $html = self::start($previousAndNext);
+
         for ($i = 1; $i <= $nbPages; $i++) {
-            $html .= "<li><a href='?page=$i'>$i</a></li>";
+            if ($active) {
+                $active = $_GET['page'] == $i ? 'class="active"' : '';
+            } else {
+                $active = "";
+            }
+            $html .= "<li $active ><a href='?page=$i'>$i</a></li>";
         }
-        $html .= '</ul>';
+
+        $html .= self::end($previousAndNext, $nbPages);
+
+        return $html;
+    }
+
+    /**
+     * Démarre la liste de pagination
+     * @param $previous
+     * @return string
+     */
+    public static function start($previous)
+    {
+        $html = "<ul>";
+        if ($previous) {
+            $page = $_GET['page'] - 1 <= 1 ? 1 : $_GET['page'] - 1;
+            if ($_GET['page'] != 1) {
+                $html .= "<li><a href='?page={$page}'><</a></li>";
+            }
+        }
+        return $html;
+    }
+
+    /**
+     * Termine la liste de pagination
+     * @param $next
+     * @param $nbPages
+     * @return string
+     */
+    public static function end($next, $nbPages)
+    {
+        $html = "";
+        if ($next) {
+            $page = $_GET['page'] + 1 >= $nbPages ? $nbPages : $_GET['page'] + 1;
+            if ($_GET['page'] != $nbPages) {
+                $html .= "<li><a href='?page={$page}'>></a></li>";
+            }
+        }
+        $html .= "</ul>";
+
         return $html;
     }
 } 
