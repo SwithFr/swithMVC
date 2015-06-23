a8e4ad3814e9ad9399c6011daf9894b01aa183fb
Jeremy Smith <contact@swith.fr>
2015-06-23 17:09:40 +0200

    add enabled languages

--- App/Config/app_config.php
+++ App/Config/app_config.php
@@ -5,6 +5,7 @@ return [
     'default_action' => 'index',
     'default_controller' => 'home',
     'prefixes' => ['admin', 'user'],
+    'languages' => ['Fr', 'En', 'Es'],
     'environments_ip' => [
         '127.0.0.1' => 'dev',
         'ip_test' => 'test',
