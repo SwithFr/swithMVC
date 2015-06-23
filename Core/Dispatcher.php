2d5da42c55630d24c418a7a3bd5ff0fcc68183ac
Jeremy Smith <contact@swith.fr>
2015-06-23 19:48:38 +0200

    syntaxe

--- Core/Dispatcher.php
+++ Core/Dispatcher.php
@@ -100,10 +100,12 @@ class Dispatcher
     private function verify()
     {
         echo "<div class='container'>";
-        if ($_ENV['SALT_KEY'] == '2129762b19c044ab7f49ea8995f7795e886ea4be')
+        if ($_ENV['SALT_KEY'] == '2129762b19c044ab7f49ea8995f7795e886ea4be') {
             echo "<div class='alert alert-warning'>Pensez à bien modifier la clé de sécurité dans le fichier <code>App/Config/*.env</code> correspondant à votre environnment actuel</div>";
-        if ($_ENV['DB_HOST'] == 'host_name' || $_ENV['DB_LOGIN'] == 'database_login')
+        }
+        if ($_ENV['DB_HOST'] == 'host_name' || $_ENV['DB_LOGIN'] == 'database_login') {
             echo "<div class='alert alert-warning'>Configurez votre fichier <code>App/Config/app_config.php</code> avant tout !</div>";
+        }
         echo "</div>";
 
         $this->isVerified = true;
