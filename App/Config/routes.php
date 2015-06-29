ce3430e81cb428b2e3226d2690ea3fc54bb88c6e
Jeremy Smith <contact@swith.fr>
2015-06-29 12:46:51 +0200

    add comment

--- App/Config/routes.php
+++ App/Config/routes.php
@@ -2,6 +2,12 @@
 
 use Core\Router;
 
+/**
+ * Le chemin d'accès à une page est configuré comme étant
+ * Controller/action qui lui correspond avec éventuellement quelque paramettres
+ * En utilisant les routes vous pouvez changer ce comportement
+ * en utilisant le router. Il vous suffit de suivre l'exemple ci-dessous
+ */
 Router::join('votre/url/{param1}/{param2}',[
     "controller" => 'controller',
     "action"     => 'action',
