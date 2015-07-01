41867458b087c0f3d9e25c808a04ed2b28663348
Jeremy Smith <contact@swith.fr>
2015-07-01 12:20:16 +0200

    add toSlug method

--- Core/Helpers/Text.php
+++ Core/Helpers/Text.php
@@ -23,4 +23,42 @@ class Text
         return $text;
     }
 
+    /**
+     * Transofrme une chaine de caractère en slug
+     * @param $string
+     * @return string
+     */
+    public static function toSlug($string)
+    {
+        return strtolower(str_replace(' ', '-',self::removeAccent($string)));
+    }
+
+    /**
+     * Supprime les accents d'une chaine de caractères
+     * @param $string
+     * @return string
+     */
+    public static function removeAccent($string)
+    {
+        return str_replace(
+            array(
+                'à', 'â', 'ä', 'á', 'ã', 'å',
+                'î', 'ï', 'ì', 'í',
+                'ô', 'ö', 'ò', 'ó', 'õ', 'ø',
+                'ù', 'û', 'ü', 'ú',
+                'é', 'è', 'ê', 'ë',
+                'ç', 'ÿ', 'ñ',
+            ),
+            array(
+                'a', 'a', 'a', 'a', 'a', 'a',
+                'i', 'i', 'i', 'i',
+                'o', 'o', 'o', 'o', 'o', 'o',
+                'u', 'u', 'u', 'u',
+                'e', 'e', 'e', 'e',
+                'c', 'y', 'n',
+            ),
+            $string
+        );
+    }
+
 } 
