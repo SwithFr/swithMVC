26a5085d2b77fb6fd2233654c63375b301b1b3f0
SwithFr <contact@swith.fr>
2015-07-16 16:59:56 +0200

    update default layout

--- App/Views/Layouts/default.php
+++ App/Views/Layouts/default.php
@@ -7,7 +7,11 @@
     <?= Html::css('default'); ?>
 </head>
 <body>
-
+<header>
+    <h1 class="title--center"><em>Bienvenue !</em> <br/>Bonne utilisation de Swith Framework !</h1>
+    <p class="doc-help">Si vous avez besoin d'aide, consultez la documentation ici : <a
+            href="http://mvc.swith.fr/pages/documentation/commencer" class="doc-link">Documentation</a></p>
+</header>
 <?= $this->Session->flash(); ?>
 
 <div class="container">
