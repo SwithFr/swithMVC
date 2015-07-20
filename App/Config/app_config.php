3b16e5b35fdc9f0f7e72f46ee9741f34e77fe2fa
SwithFr <contact@swith.fr>
2015-07-21 00:25:56 +0200

    add comments

--- App/Config/app_config.php
+++ App/Config/app_config.php
@@ -1,11 +1,41 @@
 <?php
 
 return [
+    /**
+     * Le nom de votre application, vous pouvez vous en servir pour le titre de vos pages par exemple
+     */
     'name' => 'Titre de mon site',
+
+    /**
+     * L'action appelée par défaut au sein de votre application
+     * (utilisé pour la page d'accueil par exemple)
+     */
     'default_action' => 'index',
+
+    /**
+     * Le controller appelé par défaut
+     * tout comme l'action, il est utilisé pour la page d'accueil
+     */
     'default_controller' => 'home',
+
+    /**
+     * Les préfixes que souhaitez rendre disponibles
+     * pratique pour limiter certaines actions
+     * à des utilisateurs dont le rôle correpond à un prefixe
+     */
     'prefixes' => ['admin', 'user'],
-    'languages' => ['Fr', 'En', 'Es'],
+
+    /**
+     * Vos environnements de développement.
+     * Vous pouvez en définir autant que vous voulez, par défaut il y en a 3
+     * si vous souhaitez en ajouter il vous faudra créer les fichiers correspondants
+     * en vous basant sur ceux déjà existants.
+     *
+     * Le principe est simple, vous définissez une IP que vous associez à un fichier
+     * d'envrionnement. Ainsi vous pouvez avoir plusieurs configurations pour
+     * la base de données et ne pas avoir à faire les modifications à chaque fois
+     * l'environnement sera choisi automatiquement avec l'IP courrante
+     */
     'environments_ip' => [
         '127.0.0.1' => 'dev',
         'ip_test' => 'test',
